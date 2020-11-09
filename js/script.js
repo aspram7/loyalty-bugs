8a41f6773829ef5ea052ddc6bbbec8a56b2005d9
Nyaki1144 <narek1144@gmail.com>
2020-11-09 19:16:28 +0400

    finish_1.3V

---
+++ js/script.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".scrollToTop").fadeIn(100);
+    } else {
+      $(".scrollToTop").fadeOut(100);
+    }
+  });
+
+  $(".scrollToTop").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 100);
+  });
+});
+
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const colorCh = document.querySelectorAll(".colorCh");
+const cancelBtn = document.querySelector(".cancel-btn");
+const smooth = document.querySelectorAll(".smooth-goto");
+const scrollblog = document.querySelectorAll(".scrblog-etc");
+
+
+menuBtn.onclick = () => {
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+  body.classList.add("disabled");
+};
+cancelBtn.onclick = () => {
+  body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+};
+smooth.forEach(element => {
+  element.onclick = () => {
+    body.classList.remove("disabled");
+    navbar.classList.remove("show");
+    menuBtn.classList.remove("hide");
+  };
+});
+scrollblog.forEach(element => {
+  element.onclick = () => {
+    body.classList.remove("disabled");
+    navbar.classList.remove("show");
+    menuBtn.classList.remove("hide");
+  };
+});
+window.onscroll = () => {
+  if (this.scrollY >= 20) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
+
+$(".smooth-goto").on("click", function () {
+  $("html, body").animate({ scrollTop: $(this.hash).offset().top - 250 }, 100);
+  return false;
+});
+
+$(".scrblog-etc").on("click", function () {
+  $("html, body").animate({ scrollTop: $(this.hash).offset().top - 110 }, 100);
+  return false;
+});
---
+++ js/slider.js
@@ -0,0 +1,30 @@
+$(function(){
+    $('.partners-items').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+
+        autoplay: true,
+        autoplaySpeed: 1500,
+        responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        slidesToShow: 1
+      }
+    }
+  ]
+
+    })
+})
